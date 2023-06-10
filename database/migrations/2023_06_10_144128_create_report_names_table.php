88cce671b42a3b8eda19bee44f01ccd4969d463a
unknown <yousefalsaadany5@gmail.com>
2023-06-10 17:43:55 +0300

    add folders

---
+++ database/migrations/2023_06_10_144128_create_report_names_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReportNamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('report_names', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->tinyInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('report_names');
+    }
+}
